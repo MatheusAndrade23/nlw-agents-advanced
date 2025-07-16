4d2c99e7e9f73e38cbe3037bd50ae9ae99884912
Matheus <matheusandrade.ma2003@gmail.com>
2025-07-15 21:11:52 -0300

    fix: add react query client provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./app.tsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 // biome-ignore lint/style/noNonNullAssertion: mandatory by react
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
